5142d480c77b00f7c07390daa6bc961ec4632919
StaubMaster <dgerold@student.42heilbronn.de>
2025-10-29 19:46:39 +0100

    new LineCommand Split that uses Pointers

--- include/Parsing/LineCommand.hpp
+++ include/Parsing/LineCommand.hpp
@@ -34,6 +34,19 @@ static void Split(const FileContext & file, ObjectType & object, void (ObjectTyp
 	}
 }
 
+template <typename ObjectType>
+static ObjectType * Split(const FileContext & file, ObjectType * object, ObjectType * (*func)(ObjectType *, const LineCommand &))
+{
+	std::stringstream stream(file.LoadText());
+	std::string segment;
+	while (std::getline(stream, segment, '\r'))
+	{
+		segment = StringHelp::RemoveFromString(segment, StringHelp::CharPallet("\n\r"));
+		segment = StringHelp::RemoveFromString(segment, StringHelp::CharPallet("#"), StringHelp::CharPallet("\n"));
+		object = (*func)(object, LineCommand(segment));
+	}
+	return object;
+}
 
 
 };
